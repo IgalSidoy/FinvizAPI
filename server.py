0d982d7ce6253a7b238e0ad3129bc49c9aab8729
igal sidoy <igal.sidoy@gmail.com>
2021-07-04 23:27:35 +0300

    fix some issue with heroku deployment

--- server.py
+++ server.py
@@ -5,26 +5,26 @@ import src.get as get
 import os
 
 
-app = Flask(__name__)
+finviz = Flask(__name__)
 
 
-@app.route('/scrapper/data')
+@finviz.route('/scrapper/data')
 def get_stock_data():
     ticker = request.args.get('ticker')
     result = get.get_ticker_data(ticker)
     return result
 
 
-@app.route('/scrapper/screeners')
+@finviz.route('/scrapper/screeners')
 def get_screeners():
     result = get.get_screeners()
     return json.dumps(result)
 
 
-@app.route('/health')
+@finviz.route('/health')
 def health():
-
     return {"status": "online"}
 
 
-app.run()
+if __name__ == '__main__':
+    finviz.run(debug=True)
